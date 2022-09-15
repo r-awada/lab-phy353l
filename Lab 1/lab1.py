f4e8e5688bfabd2f91ecc5a9322c7fea88403930
r-awada <r.awada@utexas.edu>
2022-09-15 17:03:17 -0500

    Fun edits

--- Lab 1/lab1.py
+++ Lab 1/lab1.py
@@ -4,53 +4,42 @@ import matplotlib.pyplot as plt
 
 def FUN():
     tube1 = pd.read_csv("tube1.csv")
-    print('Imported tube1')
-    hd_wave_1 = tube1['HD spec 1 wave'].tolist()
-    hd_intensity_1 = tube1['HD spec 1 intensity'].tolist()
-    hd_wave_2 = tube1['HD spec 2 wave'].tolist()
-    hd_intensity_2 = tube1['HD spec 2 intensity'].tolist()
-    hd_wave_3 = tube1['HD spec 3 wave'].tolist()
-    hd_intensity_3 = tube1['HD spec 3 intensity'].tolist()
-    print('made all lists for tube1')
+    tube2 = pd.read_csv("tube2.csv")
+    table1 = dict()
+    table2 = dict()
+    for i in range(1,4):
+        table1[f"hd_wave_{i}"] = tube1[f'HD spec {i} wave'].tolist()
+        table1[f"hd_intensity_{i}"] = tube1[f'HD spec {i} intensity'].tolist()
+        table2[f"h_wave_{i}"] = tube2[f'H spec {i} wave'].tolist()
+        table2[f"h_intensity_{i}"] = tube2[f'H spec {i} intensity'].tolist()
 
-    tube1 = pd.read_csv("tube2.csv")
-    print('Imported tube2')
-    h_wave_1 = tube1['H spec 1 wave'].tolist()
-    h_intensity_1 = tube1['H spec 1 intensity'].tolist()
-    h_wave_2 = tube1['H spec 2 wave'].tolist()
-    h_intensity_2 = tube1['H spec 2 intensity'].tolist()
-    h_wave_3 = tube1['H spec 3 wave'].tolist()
-    h_intensity_3 = tube1['H spec 3 intensity'].tolist()
-    print('made all lists for tube1')
-
-    plt.plot(hd_wave_1, hd_intensity_1, color='green', linestyle='dashed',linewidth=1)
-    plt.plot(hd_wave_2, hd_intensity_2, color='blue', linestyle='dashed',linewidth=1)
-    plt.plot(hd_wave_3, hd_intensity_3, color='red', linestyle='dashed',linewidth=1)
+    colors=['green','blue','red', 'orange']
+    labels=['trial 1', 'trial 2', 'trial 3']
+    for i in range(1,4):
+        plt.plot(table1[f"hd_wave_{i}"], table1[f"hd_intensity_{i}"], color=colors[i-1], linestyle='dashed',linewidth=1, label=labels[i-1])
     print('made plt for tube1')
+    plt.title('HD Intensity vs Wavelength for Tube 1')
+    plt.xlabel('Wavelength (nm)')
+    plt.ylabel('Intensity')
+    plt.legend(loc="upper left")
     plt.show()
-
-    plt.plot(h_wave_1, h_intensity_1, color='green', linestyle='dashed',linewidth=1)
-    plt.plot(h_wave_2, h_intensity_2, color='blue', linestyle='dashed',linewidth=1)
-    plt.plot(h_wave_3, h_intensity_3, color='red', linestyle='dashed',linewidth=1)
+    for i in range(1,4):
+        plt.plot(table2[f"h_wave_{i}"], table2[f"h_intensity_{i}"], color=colors[i-1], linestyle='dashed',linewidth=1, label=labels[i-1])
     print('made plt for tube2')
-    plt.show()
-
-    plt.plot(hd_wave_1, hd_intensity_1,'r--', label='HD')
-    plt.plot( h_wave_1, h_intensity_1, 'b--', label= 'H')
+    plt.title('H Intensity vs Wavelength Tube 2')
+    plt.xlabel('Wavelength (nm)')
+    plt.ylabel('Intensity')
     plt.legend(loc="upper left")
     plt.show()
-    print('Plotted both samples or smt idk dawg')
 
-    plt.plot(hd_wave_2, hd_intensity_2,'r--', label='HD')
-    plt.plot( h_wave_2, h_intensity_2, 'b--', label= 'H')
-    plt.legend(loc="upper left")
-    plt.show()
-    print('Plotted both samples or smt idk dawg')
-
-    plt.plot(hd_wave_3, hd_intensity_3,'r--', label='HD')
-    plt.plot( h_wave_3, h_intensity_3, 'b--', label= 'H')
-    plt.legend(loc="upper left")
-    plt.show()
+    for i in range(1,4):
+        plt.plot(table2[f"h_wave_{i}"], table2[f"h_intensity_{i}"], color=colors[i-1], linestyle='dashed',linewidth=1, label='H')
+        plt.plot(table1[f"hd_wave_{i}"], table1[f"hd_intensity_{i}"], color=colors[i], linestyle='dashed',linewidth=1, label='HD')
+        plt.title(f'Trial {i} Intensity vs Wavelength for H and HD')
+        plt.xlabel('Wavelength (nm)')
+        plt.ylabel('Intensity')
+        plt.legend(loc="upper left")
+        plt.show()
     print('Plotted both samples or smt idk dawg')
     return
 
