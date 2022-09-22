a4621269c956108b0c763b2e45b01a2c148ab2c8
r-awada <r.awada@utexas.edu>
2022-09-22 17:06:50 -0500

    Fun Stuff

--- Lab 1/lab1.py
+++ Lab 1/lab1.py
@@ -13,6 +13,7 @@ def gaus2(x, a, b, c, d, e, f):
 
 
 def FUN():
+    # Import all data
     tube1 = pd.read_csv("tube1trunc.csv")
     tube2 = pd.read_csv("tube2trunc.csv")
     tube3 = pd.read_csv("tube3trunc.csv")
@@ -21,6 +22,7 @@ def FUN():
     table2 = dict()
     table3 = dict()
     table4 = dict()
+    # Save all data trials into tables of tube
     for i in range(1, 4):
         table1[f"hd_wave_{i}"] = tube1[f'HD spec {i} wave'].tolist()
         table1[f"hd_intensity_{i}"] = tube1[f'HD spec {i} intensity'].tolist()
@@ -30,92 +32,139 @@ def FUN():
         table3[f"hd_intensity_{i}"] = tube3[f'HD spec {i} intensity'].tolist()
         table4[f"h_wave_{i}"] = tube4[f'H spec {i} wave'].tolist()
         table4[f"h_intensity_{i}"] = tube4[f'H spec {i} intensity'].tolist()
+    x = table1['hd_wave_1']
+    
+    # Compute average of each tube
     table1['Average Intensity'] = tube1['HD average intensity'].tolist()
     table2['Average Intensity'] = tube2['H average intensity'].tolist()
     table3['Average Intensity'] = tube3['HD average intensity'].tolist()
     table4['Average Intensity'] = tube4['H average intensity'].tolist()
+
     colors = ['green', 'blue', 'red', 'orange']
     labels = ['trial 1', 'trial 2', 'trial 3']
 
-    plt.plot(table1['hd_wave_1'], table1['Average Intensity'],
-             'gx', label='Tube 1')
-    plt.plot(table1['hd_wave_1'], table2['Average Intensity'],
-             'r^', label='Tube 2')
-    plt.plot(table1['hd_wave_1'], table3['Average Intensity'],
-             'b*', label='Tube 3')
-    plt.plot(table1['hd_wave_1'], table4['Average Intensity'],
+
+    pguess4 = [5900, 655.82, 5E-2]
+    pguess2 = [5900, 655.82, 5E-2]
+    pguess1 = [5900, 655.82, 5E-2, 3000,654,5E-2]
+    params4, pcov4 = spy.curve_fit(
+        gaus,x, table4['Average Intensity'], p0=pguess4)
+    params2, pcov2 = spy.curve_fit(
+        gaus,x, table2['Average Intensity'], p0=pguess2)
+    params1, pcov1 = spy.curve_fit(
+        gaus2,x, table1['Average Intensity'], p0=pguess1)
+    print(params2, pcov2)
+    guess2 = []
+    func2 = []
+    func4 = []
+    guess4 = []
+    func1 = []
+    guess1 = []
+    # Compute y values for guess and model
+    for i in x:
+        guess2.append(gaus(i, *pguess2))
+        func2.append(gaus(i, *params2))
+        guess4.append(gaus(i, *pguess4))
+        func4.append(gaus(i, *params4))
+        guess1.append(gaus2(i, *pguess1))
+        func1.append(gaus2(i, *params1))
+
+
+    #Plot model and guess and data 
+    plt.plot(x, func4, 'b--', label="Function Fun time")
+    plt.plot(x, guess4, 'r--', label='Guess')
+    plt.plot(x, table4['Average Intensity'],
              'yo', label='Tube 4')
-    plt.title('Average Intensity of All Tubes')
+    plt.legend(loc="upper left")
+    plt.title('Average Intensity of Tube4')
     plt.xlabel('Wavelength (nm)')
     plt.ylabel('Intensity')
+    print('Guess for tube 4 \n', params4, pcov4)
+    plt.show()
+    
+    plt.plot(x, func2, 'b--', label="Function Fun time")
+    plt.plot(x, guess2, 'r--', label='Guess')
+    plt.plot(x, table2['Average Intensity'],
+             'yo', label='Tube 2')
     plt.legend(loc="upper left")
+    plt.title('Average Intensity of Tube2')
+    plt.xlabel('Wavelength (nm)')
+    plt.ylabel('Intensity')
+    print('Guess for tube2 \n', params2, pcov2)
     plt.show()
 
-    pguess = [5100, 655.8, 2E-2]
-    params, pcov = spy.curve_fit(
-        gaus, table1['hd_wave_1'], table4['Average Intensity'], p0=pguess)
-    print(params, pcov)
-    func = []
-    guess = []
+    plt.plot(x, func1, 'b--', label="Function Fun time")
+    plt.plot(x, guess1, 'r--', label='Guess')
+    plt.plot(x, table1['Average Intensity'],
+             'yo', label='Tube 1')
+    plt.legend(loc="upper left")
+    plt.title('Average Intensity of Tube1')
+    plt.xlabel('Wavelength (nm)')
+    plt.ylabel('Intensity')
+    print('Guess for tube1 \n', params1, pcov1)
+    plt.show()
+   # plt.plot(table1['hd_wave_1'], table1['Average Intensity'],
+   #          'gx', label='Tube 1')
+   # plt.plot(table1['hd_wave_1'], table2['Average Intensity'],
+   #          'r^', label='Tube 2')
+   # plt.plot(table1['hd_wave_1'], table3['Average Intensity'],
+   #          'b*', label='Tube 3')
+   # plt.plot(table1['hd_wave_1'], table4['Average Intensity'],
+   #          'yo', label='Tube 4')
+   # plt.title('Average Intensity of All Tubes')
+   # plt.xlabel('Wavelength (nm)')
+   # plt.ylabel('Intensity')
+   # plt.legend(loc="upper left")
+   # plt.show()
+   # for i in range(1, 4):
+   #     plt.plot(table1[f"hd_wave_{i}"], table1[f"hd_intensity_{i}"],
+   #              color=colors[i-1], linestyle='dashed', linewidth=1, label=labels[i-1])
+   # print('made plt for tube1')
+   # plt.title('HD Intensity vs Wavelength for Tube 1')
+   # plt.xlabel('Wavelength (nm)')
+   # plt.ylabel('Intensity')
+   # plt.legend(loc="upper left")
+   # plt.show()
+   # for i in range(1, 4):
+   #     plt.plot(table2[f"h_wave_{i}"], table2[f"h_intensity_{i}"],
+   #              color=colors[i-1], linestyle='dashed', linewidth=1, label=labels[i-1])
+   # print('made plt for tube2')
+   # plt.title('H Intensity vs Wavelength Tube 2')
+   # plt.xlabel('Wavelength (nm)')
+   # plt.ylabel('Intensity')
+   # plt.legend(loc="upper left")
+   # plt.show()
 
-    for i in table4['h_wave_1']:
-        guess.append(gaus(i, *pguess))
-        func.append(gaus(i, *params))
-    plt.plot(table4['h_wave_1'], func, 'b--', label="Function Fun time")
-    plt.plot(table4['h_wave_1'], guess, 'r--', label='Guess')
-    plt.plot(table4['h_wave_1'], table4['Average Intensity'],
-             'yo', label='Tube 4')
-    plt.legend(loc="upper left")
-    plt.show()
-    for i in range(1, 4):
-        plt.plot(table1[f"hd_wave_{i}"], table1[f"hd_intensity_{i}"],
-                 color=colors[i-1], linestyle='dashed', linewidth=1, label=labels[i-1])
-    print('made plt for tube1')
-    plt.title('HD Intensity vs Wavelength for Tube 1')
-    plt.xlabel('Wavelength (nm)')
-    plt.ylabel('Intensity')
-    plt.legend(loc="upper left")
-    plt.show()
-    for i in range(1, 4):
-        plt.plot(table2[f"h_wave_{i}"], table2[f"h_intensity_{i}"],
-                 color=colors[i-1], linestyle='dashed', linewidth=1, label=labels[i-1])
-    print('made plt for tube2')
-    plt.title('H Intensity vs Wavelength Tube 2')
-    plt.xlabel('Wavelength (nm)')
-    plt.ylabel('Intensity')
-    plt.legend(loc="upper left")
-    plt.show()
+   # for i in range(1, 4):
+   #     plt.plot(table3[f"hd_wave_{i}"], table3[f"hd_intensity_{i}"],
+   #              color=colors[i-1], linestyle='dashed', linewidth=1, label=labels[i-1])
+   # print('made plt for tube3')
+   # plt.title('H Intensity vs Wavelength Tube 3')
+   # plt.xlabel('Wavelength (nm)')
+   # plt.ylabel('Intensity')
+   # plt.legend(loc="upper left")
+   # plt.show()
 
-    for i in range(1, 4):
-        plt.plot(table3[f"hd_wave_{i}"], table3[f"hd_intensity_{i}"],
-                 color=colors[i-1], linestyle='dashed', linewidth=1, label=labels[i-1])
-    print('made plt for tube3')
-    plt.title('H Intensity vs Wavelength Tube 3')
-    plt.xlabel('Wavelength (nm)')
-    plt.ylabel('Intensity')
-    plt.legend(loc="upper left")
-    plt.show()
-
-    for i in range(1, 4):
-        plt.plot(table4[f"h_wave_{i}"], table4[f"h_intensity_{i}"],
-                 color=colors[i-1], linestyle='dashed', linewidth=1, label=labels[i-1])
-    print('made plt for tube4')
-    plt.title('H Intensity vs Wavelength Tube 4')
-    plt.xlabel('Wavelength (nm)')
-    plt.ylabel('Intensity')
-    plt.legend(loc="upper left")
-    plt.show()
-    for i in range(1, 4):
-        plt.plot(table2[f"h_wave_{i}"], table2[f"h_intensity_{i}"],
-                 color=colors[i-1], linestyle='dashed', linewidth=1, label='H')
-        plt.plot(table1[f"hd_wave_{i}"], table1[f"hd_intensity_{i}"],
-                 color=colors[i], linestyle='dashed', linewidth=1, label='HD')
-        plt.title(f'Trial {i} Intensity vs Wavelength for H and HD')
-        plt.xlabel('Wavelength (nm)')
-        plt.ylabel('Intensity')
-        plt.legend(loc="upper left")
-        plt.show()
-    print('Plotted both samples or smt idk dawg')
+   # for i in range(1, 4):
+   #     plt.plot(table4[f"h_wave_{i}"], table4[f"h_intensity_{i}"],
+   #              color=colors[i-1], linestyle='dashed', linewidth=1, label=labels[i-1])
+   # print('made plt for tube4')
+   # plt.title('H Intensity vs Wavelength Tube 4')
+   # plt.xlabel('Wavelength (nm)')
+   # plt.ylabel('Intensity')
+   # plt.legend(loc="upper left")
+   # plt.show()
+   # for i in range(1, 4):
+   #     plt.plot(table2[f"h_wave_{i}"], table2[f"h_intensity_{i}"],
+   #              color=colors[i-1], linestyle='dashed', linewidth=1, label='H')
+   #     plt.plot(table1[f"hd_wave_{i}"], table1[f"hd_intensity_{i}"],
+   #              color=colors[i], linestyle='dashed', linewidth=1, label='HD')
+   #     plt.title(f'Trial {i} Intensity vs Wavelength for H and HD')
+   #     plt.xlabel('Wavelength (nm)')
+   #     plt.ylabel('Intensity')
+   #     plt.legend(loc="upper left")
+   #     plt.show()
+   # print('Plotted both samples or smt idk dawg')
     return
 
 
