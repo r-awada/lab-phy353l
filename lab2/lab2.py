1d80778e53c933f859ea7236070fb397dfe331e7
camibrown <camillebrown@wireless-10-146-189-70.public.utexas.edu>
2022-09-30 00:57:44 -0500

    CAMI IS A GENIUS

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -8,15 +8,19 @@ def LABLAB():
     #import data for the fun of it
     tall1 = pd.read_csv('tall50.csv')
     tall2 = pd.read_csv('tall100.csv')
+    tall3 = pd.read_csv('tall1000.csv')
 
     tall50counts = dict()
     tall100counts = dict()
+    tall1000counts = dict()
 
     for i in range(1,4):
         tall50counts[i] = tall1[f"count{i}"].tolist()
         tall100counts[i] = tall2[f"count{i}"].tolist()
+        tall1000counts[i] = tall3[f"count{i}"].tolist()
         hist = plt.hist(tall50counts[i], bins=max(tall1[f"count{i}"]))
         hist = plt.hist(tall100counts[i], bins=max(tall2[f"count{i}"]))
+        hist = plt.hist(tall1000counts[i], bins=max(tall3[f"count{i}"]))
         plt.ylabel('Occurance of Counts')
         plt.xlabel('Count within wait 50 ms')
         plt.show()
