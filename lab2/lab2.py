84f6ae4e1b4947748608aa3b0a575c71884011db
r-awada <r.awada@utexas.edu>
2022-10-06 17:28:49 -0500

    NEW DATA

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -26,54 +26,71 @@ def LABLAB():
         return (np.exp(-1* mu)*mu**x)/recur2(x)
     #import data for the fun of it
     tall1 = pd.read_csv('tall50.csv')
-    tall2 = pd.read_csv('tall100.csv')
+    tall2 = pd.read_csv('tall500.csv')
     tall3 = pd.read_csv('tall1000.csv')
     short1= pd.read_csv('short50.csv')
     short2= pd.read_csv('short500.csv')
+    short3= pd.read_csv('short1000.csv')
 
-    tall50counts = {'title':'Tall Occurance of Counts with 50 ms Integration Time over 10 s', "guess":[3]}
-    tall100counts = {'title':'Tall Occurance of Counts with 500 ms Integration Time over 100 s', "guess":[5]}
-    tall1000counts = {'title':'Tall Occurance of Counts with 1s Integration Time over 200 s', "guess":[20]}
-    short50counts = {'title':'Short Occurance of Counts with 50 ms Integration Time over 10 s',"guess":[17]}
-    short500counts= {'title':'Short Occurance of Counts with 500 ms Integration Time over 100 s',"guess":[150]}
+    tall50counts = {'title':'Tall Occurance of Counts with 50 ms Integration Time of 10 s', "guess":[1]}
+    tall500counts = {'title':'Tall Occurance of Counts with 500 ms Integration Time of 100 s', "guess":[3]}
+    tall1000counts = {'title':'Tall Occurance of Counts with 1s Integration Time of 200 s', "guess":[23]}
+    short50counts = {'title':'Short Occurance of Counts with 50 ms Integration Time of 10 s',"guess":[2]}
+    short500counts= {'title':'Short Occurance of Counts with 500 ms Integration Time of 100 s',"guess":[23]}
+    short1000counts= {'title':'Short Occurance of Counts with 1s Integration Time of 100 s',"guess":[23]}
 
-    data = [tall50counts, tall100counts, tall1000counts, short50counts, short500counts]
+    data = [tall50counts, tall500counts, tall1000counts]
+    paired_data= {0:short50counts,1:short500counts,2:short1000counts}
 
-    def ploth(histo, title, binz, guess):
-        hist = plt.hist(histo,bins = binz)
-        x = [int(i) for i in hist[1]][1::]
-        y = [int(i) for i in hist[0]]
+    def ploth(histo, title, binz, guess, label0):
+        entries, bin_edges, patches= plt.hist(histo,bins = binz,density=True,label=label0)
+        #x = [int(i) for i in hist[1]][1::]
+        #norm = sum(hist[0])
+        #y = [int(i)/norm for i in hist[0]] # JEREMY HELP
+        #y0 = [i**(1/2) for i in y]
         #print(params, pcov)
         plt.ylabel('Occurance of Counts')
         plt.xlabel('Count Number measured')
         plt.title(title)
+        plt.legend(loc="upper left")
         #x = np.linspace(0,x[-1],100)
-        params, pcov = spy.curve_fit(poison, x, y,p0=guess)
-        return params, pcov, x
+        bin_middles = 0.5 * (bin_edges[1:] + bin_edges[:-1])
+        params, pcov = spy.curve_fit(poison,bin_middles, entries,p0=guess)
+        return params, pcov, entries
 
-    def plotp(x, params, title, label0):
-        x = np.linspace(0,x[-1],1000)
-        plt.plot(x,poison(x,*params), label = label0)
+    def plotp(params, title, label0, entries):
+        x_fun = np.arange(len(entries))
+        x = np.linspace(0,len(entries),100)
+        plt.plot(x,poison(x,*params),'-', label = label0)
+        #print(np.trapz(poison(x_fun,*params)))
+        plt.plot(np.arange(len(entries)),entries,"o",label= "Original"+label0 )
         plt.legend(loc="upper left")
         plt.title(title)
         
 
     for i in range(1,4):
         tall50counts[i] = tall1[f"count{i}"].tolist()
-        tall100counts[i] = tall2[f"count{i}"].tolist()
+        tall500counts[i] = tall2[f"count{i}"].tolist()
         tall1000counts[i] = tall3[f"count{i}"].tolist()
         short50counts[i] = short1[f"count{i}"].tolist()
         short500counts[i] = short2[f"count{i}"].tolist()
+        short1000counts[i] = short3[f"count{i}"].tolist()
 
-    for i in data:
-        for j in range(1,4):
-            i[f"params{j}"], i[f"pcov{j}"], i[f"x{j}"] = ploth(i[j], i['title'], max(i[j]), i['guess'])
+    for num,i in enumerate(data):
+        plt.legend(loc="upper left")
+        for j in range(1,2):
+            i[f'bins{j}'] = np.arange(max(i[j])) -0.5
+            i[f"params{j}"], i[f"pcov{j}"], i[f"entries{j}"] = ploth(i[j], i['title'], i[f'bins{j}'], i['guess'], f"Trial{j}")
+            paired_data[num][f'bins{j}'] = np.arange(max(paired_data[num][j])) -0.5
+            paired_data[num][f"params{j}"], paired_data[num][f"pcov{j}"], paired_data[num][f"entries{j}"] = ploth(paired_data[num][j], paired_data[num]['title'], paired_data[num][f'bins{j}'], paired_data[num]['guess'], f"Trial{j}")
         plt.show()
-    for i in data:
-        for j in range(1,4):
-            plotp(i[f"x{j}"], i[f"params{j}"], i['title'], f"Trial {j}")
-            #plotp(i[f"x{j}"],*i['guess'], i['title'], 'guess')
+    for num, i in enumerate(data):
+        for j in range(1,2):
+            plotp(i[f"params{j}"], i['title'], f"Trial {j}", i[f"entries{j}"])
             print(i[f"pcov{j}"])
+            plotp(paired_data[num][f"params{j}"], paired_data[num]['title'], f"Trial {j}", paired_data[num][f"entries{j}"])
+            print(paired_data[num][f"pcov{j}"])
+            #plotp(i[f"x{j}"],*i['guess'], i['title'], 'guess')
         plt.show()
 
 
