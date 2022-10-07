d8751149630a438221994a4c1aebf8da887c30bf
r-awada <r.awada@utexas.edu>
2022-10-06 23:35:52 -0500

    CAMILLE ACCENT = BAD

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -4,6 +4,7 @@ import scipy.optimize as spy
 from scipy.stats import poisson
 import numpy as np
 import scipy
+import scipy.stats as stats
 
 def LABLAB():
 
@@ -28,35 +29,26 @@ def LABLAB():
     data = [tall50counts, tall500counts, tall1000counts]
     paired_data= {0:short50counts,1:short500counts,2:short1000counts}
 
-    def chi_dsit(chi,nu):
-        return (chi**(-1/2) * np.exp(-chi/2))/(2**(1/2) * scipy.special.gamma(1/2))
 
-    def chi_squared(y_pois, y_val, sigma, N):
-        ans = 0
-        for i in y_val:
-            ans += 1/sigma * (i-poison(i,y_pois))**(2)
-        return ans
+
     def ploth(histo, title, binz, guess, label0):
         h = np.histogram(histo,bins=binz)
         entries, bin_edges, patches= plt.hist(histo,bins = binz,density=True,label=label0)
         uncer = [i**(1/2)/sum(h[0]) for i in h[0]]
-        print(uncer)
         plt.ylabel('Occurance of Counts')
         plt.xlabel('Count Number measured')
         plt.title(title)
         plt.legend(loc="upper left")
         bin_middles = 0.5 * (bin_edges[1:] + bin_edges[:-1])
-        params, pcov = spy.curve_fit(poison,bin_middles, entries,p0=guess,sigma=uncer)
-        print(pcov)
+        params, pcov = spy.curve_fit(poison,bin_middles, entries,p0=guess)
         return params, pcov, entries, uncer 
 
     def plotp(params, title, label0, entries, uncer):
         x_fun = np.arange(len(entries))
         x = np.linspace(0,len(entries),100)
         plt.plot(x,poison(x,*params),'-', label = label0)
-        #print(np.trapz(poison(x_fun,*params)))
         plt.plot(np.arange(len(entries)),entries,"o",label= "Original"+label0 )
-        #plt.errorbar(np.arange(len(entries)),entries, yerr= uncer )
+        plt.errorbar(np.arange(len(entries)),entries, yerr= uncer, linestyle='' )
         plt.legend(loc="upper left")
         plt.title(title)
         
@@ -76,15 +68,41 @@ def LABLAB():
             i[f"params{j}"], i[f"pcov{j}"], i[f"entries{j}"], i[f"uncer{j}"] = ploth(i[j], i['title'], i[f'bins{j}'], i['guess'], f"Trial{j}")
             paired_data[num][f'bins{j}'] = np.arange(max(paired_data[num][j])) -0.5
             paired_data[num][f"params{j}"], paired_data[num][f"pcov{j}"], paired_data[num][f"entries{j}"], paired_data[num][f"uncer{j}"]= ploth(paired_data[num][j], paired_data[num]['title'], paired_data[num][f'bins{j}'], paired_data[num]['guess'], f"Trial{j}")
-        plt.show()
+        #plt.show()
+
+
     for num, i in enumerate(data):
         for j in range(1,2):
             plotp(i[f"params{j}"], i['title'], f"Trial {j}", i[f"entries{j}"], i[f"uncer{j}"])
             plotp(paired_data[num][f"params{j}"], paired_data[num]['title'], f"Trial {j}", paired_data[num][f"entries{j}"], paired_data[num][f"uncer{j}"])
             #plotp(i[f"x{j}"],*i['guess'], i['title'], 'guess')
-        plt.show()
+        #plt.show()
 
 
+    def chi_dsit(chi,dof):
+        return (chi**((dof/2)-1) * np.exp(-chi/2))/(2**(dof/2) * scipy.special.gamma((dof)/2))
+
+
+    def chi_squared(y_pois, y_val, sigma):
+        ans = 0
+        for num, i in enumerate(y_val):
+            if sigma[num] != 0:
+                ans += 1/sigma[num]**2 * (i-poison(i,y_pois))**(2)
+        return ans
+
+
+    for num, i in enumerate(data):
+        for j in range(1,2):
+            dof = len(i[f"entries{j}"]) - 1
+            x = np.arange(dof+50)
+            y = [chi_dsit(p,dof) for p in x]
+            y_pois = [poison(p,*i[f"params{j}"]) for p in range(len(i[f"entries{j}"]))]
+            chi = chi_squared(y_pois, i[f"entries{j}"], i[f"uncer{j}"])
+            print(chi)
+            plt.clf()
+            plt.plot(x,y)
+            plt.axvline(x = chi)
+            plt.show()
 
 
 LABLAB()
