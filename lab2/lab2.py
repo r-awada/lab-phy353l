0d2ef6d7941ee80c9de896ddadb49acca2966634
r-awada <r.awada@utexas.edu>
2022-10-06 19:20:39 -0500

    I did this thing

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -3,27 +3,13 @@ import matplotlib.pyplot as plt
 import scipy.optimize as spy
 from scipy.stats import poisson
 import numpy as np
-
+import scipy
 
 def LABLAB():
-
-    def recur2(x):
-        if x == 1:
-            return 1
-        else:
-            return x * recur2(x-1)
-
-    def recur(x):
-        if len(x) == 1:
-            return 1
-        else:
-            return x[-1] * recur(x[:-1])
 
     def poison(x, mu):
         return poisson.pmf(x, mu)
 
-    def poisson2(x, mu):
-        return (np.exp(-1* mu)*mu**x)/recur2(x)
     #import data for the fun of it
     tall1 = pd.read_csv('tall50.csv')
     tall2 = pd.read_csv('tall500.csv')
@@ -42,28 +28,35 @@ def LABLAB():
     data = [tall50counts, tall500counts, tall1000counts]
     paired_data= {0:short50counts,1:short500counts,2:short1000counts}
 
+    def chi_dsit(chi,nu):
+        return (chi**(-1/2) * np.exp(-chi/2))/(2**(1/2) * scipy.special.gamma(1/2))
+
+    def chi_squared(y_pois, y_val, sigma, N):
+        ans = 0
+        for i in y_val:
+            ans += 1/sigma * (i-poison(i,y_pois))**(2)
+        return ans
     def ploth(histo, title, binz, guess, label0):
+        h = np.histogram(histo,bins=binz)
         entries, bin_edges, patches= plt.hist(histo,bins = binz,density=True,label=label0)
-        #x = [int(i) for i in hist[1]][1::]
-        #norm = sum(hist[0])
-        #y = [int(i)/norm for i in hist[0]] # JEREMY HELP
-        #y0 = [i**(1/2) for i in y]
-        #print(params, pcov)
+        uncer = [i**(1/2)/sum(h[0]) for i in h[0]]
+        print(uncer)
         plt.ylabel('Occurance of Counts')
         plt.xlabel('Count Number measured')
         plt.title(title)
         plt.legend(loc="upper left")
-        #x = np.linspace(0,x[-1],100)
         bin_middles = 0.5 * (bin_edges[1:] + bin_edges[:-1])
-        params, pcov = spy.curve_fit(poison,bin_middles, entries,p0=guess)
-        return params, pcov, entries
+        params, pcov = spy.curve_fit(poison,bin_middles, entries,p0=guess,sigma=uncer)
+        print(pcov)
+        return params, pcov, entries, uncer 
 
-    def plotp(params, title, label0, entries):
+    def plotp(params, title, label0, entries, uncer):
         x_fun = np.arange(len(entries))
         x = np.linspace(0,len(entries),100)
         plt.plot(x,poison(x,*params),'-', label = label0)
         #print(np.trapz(poison(x_fun,*params)))
         plt.plot(np.arange(len(entries)),entries,"o",label= "Original"+label0 )
+        #plt.errorbar(np.arange(len(entries)),entries, yerr= uncer )
         plt.legend(loc="upper left")
         plt.title(title)
         
@@ -80,16 +73,14 @@ def LABLAB():
         plt.legend(loc="upper left")
         for j in range(1,2):
             i[f'bins{j}'] = np.arange(max(i[j])) -0.5
-            i[f"params{j}"], i[f"pcov{j}"], i[f"entries{j}"] = ploth(i[j], i['title'], i[f'bins{j}'], i['guess'], f"Trial{j}")
+            i[f"params{j}"], i[f"pcov{j}"], i[f"entries{j}"], i[f"uncer{j}"] = ploth(i[j], i['title'], i[f'bins{j}'], i['guess'], f"Trial{j}")
             paired_data[num][f'bins{j}'] = np.arange(max(paired_data[num][j])) -0.5
-            paired_data[num][f"params{j}"], paired_data[num][f"pcov{j}"], paired_data[num][f"entries{j}"] = ploth(paired_data[num][j], paired_data[num]['title'], paired_data[num][f'bins{j}'], paired_data[num]['guess'], f"Trial{j}")
+            paired_data[num][f"params{j}"], paired_data[num][f"pcov{j}"], paired_data[num][f"entries{j}"], paired_data[num][f"uncer{j}"]= ploth(paired_data[num][j], paired_data[num]['title'], paired_data[num][f'bins{j}'], paired_data[num]['guess'], f"Trial{j}")
         plt.show()
     for num, i in enumerate(data):
         for j in range(1,2):
-            plotp(i[f"params{j}"], i['title'], f"Trial {j}", i[f"entries{j}"])
-            print(i[f"pcov{j}"])
-            plotp(paired_data[num][f"params{j}"], paired_data[num]['title'], f"Trial {j}", paired_data[num][f"entries{j}"])
-            print(paired_data[num][f"pcov{j}"])
+            plotp(i[f"params{j}"], i['title'], f"Trial {j}", i[f"entries{j}"], i[f"uncer{j}"])
+            plotp(paired_data[num][f"params{j}"], paired_data[num]['title'], f"Trial {j}", paired_data[num][f"entries{j}"], paired_data[num][f"uncer{j}"])
             #plotp(i[f"x{j}"],*i['guess'], i['title'], 'guess')
         plt.show()
 
