030e00d22b45eb1b200de4cd05f35f02159bf9ba
r-awada <r.awada@utexas.edu>
2022-10-18 21:32:20 -0500

    I want queso :(

--- lab3/droplet.py
+++ lab3/droplet.py
@@ -8,17 +8,17 @@ class drop:
         self.temp = temp + 273
         self.density = 886 #kg/m^3
         self.V = voltage
-        self.d = 7e-4
+        self.d = 7.5e-4
 
 
     def fall_vel(self):
         avg_fall = sum(self.fall)/len(self.fall)
-        distance = (self.dist / 16.7) * 0.0001 #convert pixels to meters
+        distance = (self.dist / 16.7) * 0.00001 #convert pixels to meters
         self.fall_vel = distance/avg_fall
 
     def rise_vel(self):
         avg_rise = sum(self.rise)/len(self.rise)
-        distance = (self.dist / 16.7) * 0.0001 #convert pixels to meters
+        distance = (self.dist / 16.7) * 0.00001 #convert pixels to meters
         self.rise_vel = distance/avg_rise
 
     def drag(self):
@@ -28,10 +28,11 @@ class drop:
         r = ((9*self.n*self.fall_vel)/(2*self.density*9.81))**(1/2)
         self.neff = self.n*(1/(1+((8.2e-3)/(101325*r))))
         self.r = r * (1/(1+(8.2e-3)/101325*r))**(1/2)
-        #self.r = ((9*self.neff*self.fall_vel)/(2*self.density*9.81))**(1/2)
+        self.r = ((9*self.neff*self.fall_vel)/(2*self.density*9.81))**(1/2)
 
     def charge(self):
         self.q = (4*self.d*math.pi*self.r**3 * self.density * 9.81 * (self.fall_vel + self.rise_vel))/(3*self.fall_vel*self.V)
+        print(self.q/1.602e-19)
 
     def charge_per_mass(self):
         self.qpm = self.q/(self.density * 4/3 * math.pi * self.r**3)
