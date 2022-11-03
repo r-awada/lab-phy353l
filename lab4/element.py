6d7c4c2358b55ad7df08618d883fc4e67e37fd12
r-awada <r.awada@utexas.edu>
2022-11-02 19:33:18 -0500

    iadskjfasdkfjaklsdfklasdfk

--- lab4/element.py
+++ lab4/element.py
@@ -8,26 +8,42 @@ class element:
     def axis(self):
         maxi = max(self.y)
         self.loc = self.y.index(maxi)
-        scale = self.peak/self.loc
+        self.scale = self.peak/self.loc
         self.x = []
         for i in range(len(self.y)):
-            self.x.append(i*scale)
+            self.x.append(i*self.scale)
+            #self.x.append(i)
 
-    def compton(self):
-        if self.loc - 150 < 0:
-            loc = 0
-        else:
-            loc = self.loc - 150
+    def compton(self, start, stop, start2, stop2):
+        topAvg = sum(self.y[start:stop])/(stop-start)
+        botAvg = sum(self.y[start2:stop2])/(stop2-start2)
+        avg = (topAvg+botAvg)/2
+        mini_dist = 999999999999898898989898989898
+        final_pos = 0
+        for i in range(start, stop2+1):
+            if abs(avg - self.y[i]) < mini_dist:
+                mini_dist = abs(avg - self.y[i])
+                final_pos = i
 
-        top = 0
-        postop = 0
-        bottom = 99999999999
-        for i in range(loc, self.loc):
-            if self.y[i] > top:
-                top = self.y[i]
-                postop = i
-            if self.y[i] < self.y[i+1] and self.y[i+1] < self.y[i+2]:
-                bottom = i
+        self.comp = final_pos * self.scale
 
-        self.comp = top +(bottom - top)/2
-        print(self.comp)
+    def compton2(self, start, stop, start2, stop2):
+        topAvg = sum(self.y[start:stop])/(stop-start)
+        botAvg = sum(self.y[start2:stop2])/(stop2-start2)
+        avg = (topAvg+botAvg)/2
+        mini_dist = 999999999999898898989898989898
+        final_pos = 0
+        for i in range(start, stop2+1):
+            if abs(avg - self.y[i]) < mini_dist:
+                mini_dist = abs(avg - self.y[i])
+                final_pos = i
+
+        self.comp2 = final_pos * self.scale
+
+    def rela(self, e):
+        k = self.comp * 1.6022e-16
+        m = 9.1093837e-31
+        c = 299792458
+        e *= 1.6022e-16
+        p = 2*(e)/c - k/c
+        return (p**2)/(2*m)
--- lab4/lab4.py
+++ lab4/lab4.py
@@ -9,10 +9,17 @@ import scipy.stats as stats
 import math
 
 
+def relativity2(k):
+    m = 9.1093837e-31
+    c = 299792458
+    return (k**2)/(2*m*c**2) + k
+
+
 def main():
 
     names = ["Na22", "Cs137", "Co60", "Co57", "Mn54", "Ba133"]
-    peaks = [511,661.657,1332.492,122.06065,834.848,356.0129]
+    peaks = [511, 661.657, 1332.492, 122.06065, 834.848, 356.0129]
+    peaks2 = [1274.537, 0, 1173.228, 0, 0, 80.9979]
     data = pd.read_csv('data.csv')
     elements = []
     for num, i in enumerate(names):
@@ -20,16 +27,45 @@ def main():
         splice = 0
         while y.index(max(y[splice:])) < 29:
             splice += 1
-        elements.append(element(i,y[splice:],peaks[num]))
+        elements.append(element(i, y[splice:], peaks[num]))
+        elements[num].axis()
+
+    elements[0].compton(45, 65, 87, 97)
+    elements[0].compton2(236, 258, 273, 289)
+    elements[1].compton(84, 107, 128, 136)
+    elements[2].compton(185, 208, 225, 235)
+    elements[2].compton2(289, 292, 293, 301)
+    elements[4].compton(109, 144, 167, 183)
+    elements[5].compton(14, 19, 23, 31)
 
     for i in elements:
-        i.axis()
-        i.compton()
-        plt.plot(i.x,i.y)
+        plt.plot(i.x, i.y)
         plt.title(i.name)
-        plt.axvline(x=i.comp, color="black")
+        try:
+            plt.axvline(x=i.comp, color="black")
+            plt.axvline(x=i.comp2, color="black")
+        except:
+            pass
         plt.show()
 
+    x = np.linspace(0, 2e-13, 100000)
+    y = [relativity2(i) for i in x]
+    plt.plot(x, y, label="Relativity")
+    plt.xlabel('Energy')
+    plt.ylabel('P^2/2m')
+    plt.title('Energy Momentum Relationship')
+    for num, i in enumerate(elements):
+        try:
+            # print(f"{i.name} has p^2 of {relativity2(i.comp)}")
+            plt.plot(i.comp*1.6022e-16, i.rela(peaks[num]),
+                     marker="o", markersize=10, label=i.name)
+        except Exception as e:
+            print(e)
+            pass
+
+    plt.legend(loc="upper left")
+    plt.plot(x, x, label="Newton")
+    plt.show()
 
 
 main()
